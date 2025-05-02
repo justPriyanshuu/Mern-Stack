2b3bb7e57dc514d140a83177dc038015da535f5c
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-05-02 11:27:29 +0530

    nested

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,16 @@
 const student={
-    Name:"Priyanshu",
-    Roll:122,
-    Section: "B1"
+    priyanshu:{
+        roll:1,
+        marks:80
+    },
+    ritik:{
+        roll:2,
+        marks:80
+    },
+    sanket:{
+        roll:3,
+        marks:80
+    },
 };
 
-console.log(student);
+console.log(student.ritik.marks);
