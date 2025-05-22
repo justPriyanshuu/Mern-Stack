3cff140514e45ef5edf9269855f862aa63f72123
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-05-22 23:52:00 +0530

    Update

--- Simon_Game/app.js
+++ Simon_Game/app.js
@@ -4,9 +4,32 @@ let userSeq=[];
 let started=false;
 let level=0;
 
+let h4=document.querySelector("h4");
+
+let btns=["one","two","three","four"];
+
 document.addEventListener("keypress",function(){
     if(started==false){
         console.log("Game started");
         started = true;
+        levelUp();
     }
 });
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },500);
+}
+
+function levelUp(){
+    level++;
+    h4.innerText=`Level ${level}`;
+
+    let randIdx=Math.floor(Math.random()*3);
+    let randColor=btns[randIdx];
+    let randbtn=document.querySelector(`.${randColor}`);
+
+    btnFlash(randbtn);
+}
