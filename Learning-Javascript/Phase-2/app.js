3bcbd90ef0802b6cf3402f85622eb5e72a9a5625
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-01 12:25:25 +0530

    Update

--- Learning-Javascript/Phase-2/app.js
+++ Learning-Javascript/Phase-2/app.js
@@ -3,15 +3,21 @@ let clock = document.getElementById("time");
 setInterval(() => {
   let time = new Date();
   let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  let currentTime = `${hours}:${minutes}`;
+
+  if (alarmTime === currentTime) {
+    alert("Alarm Ringing!");
+  }
+
   let period = "AM";
   if (hours > 12) {
     period = "PM";
     if (hours > 12) hours -= 12;
   }
   if (hours == 0) hours = 12;
-
-  let minutes = time.getMinutes();
-  let seconds = time.getSeconds();
 
   let h = hours < 10 ? "0" + hours : hours;
   let m = minutes < 10 ? "0" + minutes : minutes;
@@ -21,3 +27,11 @@ setInterval(() => {
 
   clock.innerText = final;
 }, 1000);
+
+let alarminput = document.getElementById("alarm-time");
+let alarmbtn = document.getElementById("set-alarm");
+let alarmTime = 0;
+
+alarmbtn.addEventListener("click", () => {
+  alarmTime = alarminput.value;
+});
