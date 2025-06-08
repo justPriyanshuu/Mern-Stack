e7cc47fc17651c00a22717f033168d66eeea46bb
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-08 11:36:42 +0530

    update

---
+++ Learning-Javascript/Phase-3/app.js
@@ -0,0 +1,42 @@
+let addTask = document.querySelector(".enter-task");
+let addBtn = document.querySelector(".add-btn");
+let list = document.getElementById("taskLists");
+addBtn.addEventListener("click", () => {
+  let inputValue = addTask.value;
+  // console.log(inputValue);
+
+  let li = document.createElement("li");
+  li.className = "task-item";
+  li.innerHTML = `<li class="task-item">
+              <input
+                class="form-check-input check-task"
+                type="checkbox"
+                id="check-default"
+              />
+              <label for="check-default">
+                <span class="task-text">${inputValue}</span></label
+              >
+              <button class="btn btn-sm btn-danger delete-btn">Delete</button>
+            </li>`;
+
+  list.appendChild(li);
+
+  let checkbox = li.querySelector(".check-task");
+  let taskText = li.querySelector(".task-text");
+
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      taskText.classList.add("completed");
+    } else {
+      taskText.classList.remove("completed");
+    }
+  });
+
+  let deleteBtn = li.querySelector(".delete-btn");
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  addTask.value = "";
+});
