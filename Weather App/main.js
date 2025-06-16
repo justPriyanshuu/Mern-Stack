8fbf9fe77e0e9f2cb21d315ff3217d07e2fe6cc0
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-16 23:24:33 +0530

    update

--- Weather App/main.js
+++ Weather App/main.js
@@ -1,6 +1,16 @@
-let frm=document.querySelector("#weatherForm");
+let frm = document.querySelector("#weatherForm");
 
-frm.addEventListener("click",()=>{
-    let cityName=document.querySelector("#cityInput");
-    console.log(cityName.value);
+frm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let cityName = document.querySelector("#cityInput").value;
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=02db7f7c2643f26e3d3df6523c51a4b6&units=metric`;
+
+  try {
+    const res = await axios.get(url);
+    console.log(res.data);
+  } catch (e) {
+    console.log(e);
+  }
 });
