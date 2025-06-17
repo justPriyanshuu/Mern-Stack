614e7a3074ee69a85cc078b7db967ebb1624091b
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-17 17:42:37 +0530

    Finisheddd...

--- Quote Generator/main.js
+++ Quote Generator/main.js
@@ -5,10 +5,16 @@ btn.addEventListener("click", async () => {
 
   try {
     let res = await axios.get(url);
-    let content=res.data.content;
-    let author=res.data.author;
+    let content = res.data.content;
+    let author = res.data.author;
 
-    
+    let div = document.querySelector(".result");
+    div.innerHTML = "";
+
+    let p = document.createElement("p");
+    p.innerHTML = `${content} <br>--<b>${author}</b>`;
+
+    div.appendChild(p);
   } catch (e) {
     console.log(e);
   }
