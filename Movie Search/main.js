37316250f07c56eb3e9ea42ef1f662b632168af5
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-18 18:00:33 +0530

    Final

--- Movie Search/main.js
+++ Movie Search/main.js
@@ -6,12 +6,50 @@ btn.addEventListener("click", async () => {
 
   try {
     let result = await axios.get(url);
-    let poster=result.data.Poster;
-    let title=result.data.Title;
-    let year=result.data.year;
-    let genre=result.data.Genre;
-    let actors=result.data.Actors;
-    
+    let poster = result.data.Poster;
+    let title = result.data.Title;
+    let year = result.data.Year;
+    let genre = result.data.Genre;
+    let actors = result.data.Actors;
+    let imdb = result.data.Ratings[0].Value;
+
+    let card = document.querySelector(".result");
+    card.innerHTML = "";
+
+    let movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+
+    let cardImg = document.createElement("img");
+    cardImg.src = poster;
+
+    let movieDetails = document.createElement("div");
+    movieDetails.classList.add("movie-details");
+
+    let h3 = document.createElement("h3");
+    h3.textContent = title;
+
+    let pYear = document.createElement("p");
+    pYear.textContent = "Year: " + year;
+
+    let pGenre = document.createElement("p");
+    pGenre.textContent = "Genre: " + genre;
+
+    let pActors = document.createElement("p");
+    pActors.textContent = "Actors: " + actors;
+
+    let pImdb = document.createElement("p");
+    pImdb.textContent = "Imdb Rating:" + imdb;
+
+    movieDetails.appendChild(h3);
+    movieDetails.appendChild(pYear);
+    movieDetails.appendChild(pGenre);
+    movieDetails.appendChild(pActors);
+    movieDetails.appendChild(pImdb);
+
+    movieCard.appendChild(cardImg);
+    movieCard.appendChild(movieDetails);
+
+    card.appendChild(movieCard);
   } catch (e) {
     console.log(e);
   }
