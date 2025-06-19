fb12fe47db83a0a178502e3f13dd58864906fcd5
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-06-19 20:01:01 +0530

    Update

---
+++ Github Profile Bot/main.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector(".search");
+
+btn.addEventListener("click", async () => {
+  let username = document.querySelector(".username").value.trim();
+  let url = `https://api.github.com/users/${username}`;
+
+  try {
+    let result = await axios.get(url);
+
+    document.querySelector('.card').classList.remove('hidden');
+    document.querySelector('.avatar').src= result.data.avatar_url;
+    document.querySelector('.name').textContent = result.data.name;
+    document.querySelector('.bio').textContent = result.data.bio;
+    document.querySelector('.repos').textContent = result.data.public_repos;
+    document.querySelector('.followers').textContent = result.data.followers;
+
+  } catch (e) {
+    console.log(e);
+  }
+});
